b55de2737f88f3dda98fa0cb46b45dbb19ed8d80
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-02-06 04:19:19 +0100

    Update asm 18-02-2021 T1.asm

--- asm 18-02-2021 T1.asm
+++ asm 18-02-2021 T1.asm
@@ -1,23 +1,3 @@
-;# int processa (char num, int val){
-;#     int tmp;
-;#     if(num<58)
-;#         tmp=(num-48+val)/2;
-;#     else tmp=val;
-;#     return tmp;
-;# }
-;# main(){
-;#     char num[8];
-;#     int V[8],i,a;
-;# 
-;#     printf("Inserisci una stringa di soli numeri\n");
-;#     scanf("%s",NUM);
-;#    for(int i=0;i<strlen(NUM);i++){
-;#        printf("Inserisci un numero\n");
-;#         scanf("%d,&a);
-;#         V[i]=confronta(NUM[i],a);
-;#         printf("Valore=%d\n",V[i]);
-;#     }
-;# }
 
 .data
 ST: .space 8 ;# char NUM[8] = 8*1
